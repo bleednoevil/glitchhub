0332e87ff55b36299a40cd5476a5cdb36c2070d2
$pill Bl**d <155607938+bleednoevil@users.noreply.github.com>
2026-04-03 13:17:18 -0700

    Add utility functions to clear terminal and print repo

---
+++ utils.py
@@ -0,0 +1,11 @@
+import os
+
+def clear_terminal():
+    os.system('clear')
+
+def print_repo(repo):
+    clear_terminal()
+    print("🔥 LIVE GITHUB STREAM")
+    print("--------------------------")
+    print(f"Repo: {repo}")
+    print("\nPress a lit pad to open it.")
